395f39d477424bfdc511b3abeb471bae3f76d35f
CraigNeilan <craigneilan@gmail.com>
2022-05-24 12:04:45 +0200

    Added add collection and connetcs to database and stores collection is broken as fuck though

--- app/src/main/java/com/example/opscpart2/AddCollectionActivity.java
+++ app/src/main/java/com/example/opscpart2/AddCollectionActivity.java
@@ -1,13 +1,28 @@
 package com.example.opscpart2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AddCollectionActivity extends AppCompatActivity {
+
+    FirebaseDatabase database;
+    DatabaseReference ref;
+    EditText collectionname,goal;
+    int maxid = 0;
+    Collections collections;
+    Button btn;
 
     private Button Home;
 
@@ -15,6 +30,46 @@ public class AddCollectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_collection);
+
+        collectionname= findViewById(R.id.textName);
+        goal = findViewById(R.id.textGoal);
+        btn = findViewById(R.id.btnStore);
+
+        collections = new Collections();
+        ref = database.getInstance().getReference().child("Collection");
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    maxid = (int) snapshot.getChildrenCount();
+
+                }else{
+
+                    ///
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    btn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            collections.setName(collectionname.getText().toString());
+            collections.setName(goal.getText().toString());
+
+            ref.child(String.valueOf(maxid+1)).setValue(collections);
+
+        }
+    });
+
+
 
         Home = findViewById(R.id.btnHome);
 
---
+++ app/src/main/java/com/example/opscpart2/Collections.java
@@ -0,0 +1,24 @@
+package com.example.opscpart2;
+
+public class Collections {
+    private String name;
+    private int goal;
+
+    public Collections(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGoal() {
+        return goal;
+    }
+
+    public void setGoal(int goal) {
+        this.goal = goal;
+    }
+}
