3c2b73d7ab876ff8b551e3bedc2bc88efd210639
Stephan <swanepoelhome23@gmail.com>
2022-05-30 18:15:08 +0200

    MyAdpater

---
+++ app/src/main/java/com/example/opscpart2/MyAdpater.java
@@ -0,0 +1,54 @@
+package com.example.opscpart2;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MyAdpater extends RecyclerView.Adapter<MyAdpater.MyViewHolder> {
+    private  final List<ItemGetSet> items;
+    private final Context context;
+
+
+    public MyAdpater(List<ItemGetSet> items, Context context) {
+        this.items = items;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public MyAdpater.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MyViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_adapter_layout_items,null));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyAdpater.MyViewHolder holder, int position) {
+        ItemGetSet itemGetSet = items.get(position);
+
+
+
+        holder.Details.setText(itemGetSet.getDetails());
+        holder.Date.setText(itemGetSet.getDate());
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    static  class MyViewHolder extends RecyclerView.ViewHolder {
+        private final TextView Details,Date;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            Details = itemView.findViewById(R.id.DetailsTV);
+            Date = itemView.findViewById(R.id.DateTV);
+        }
+    }
+}
