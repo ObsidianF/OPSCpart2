d98eb91b859dfc365a14d471de0422b4ba56481d
percy-hash <percymahlathi27@gmail.com>
2022-06-02 14:37:04 +0200

    iii

--- app/src/main/java/com/example/opscpart2/AddItemActivity.java
+++ app/src/main/java/com/example/opscpart2/AddItemActivity.java
@@ -17,7 +17,7 @@ import com.google.firebase.database.ValueEventListener;
 
 public class AddItemActivity extends AppCompatActivity {
 
-    FirebaseDatabase database; //test
+    FirebaseDatabase database; //
     DatabaseReference ref;
     EditText description, dateText;
     int maxid = 0;
